40c7e1f75c11e991dda47971e0acc1d0a0312507
ilotus <jilotus@qq.com>
2024-10-10 16:12:38 +0800

    init

---
+++ internal/model/rotation.go
@@ -0,0 +1,15 @@
+package model
+
+type RotationCreateUpdateBase struct {
+	PicUrl string
+	Link   string
+	Sort   int
+}
+
+type RotationCreateInput struct {
+	RotationCreateUpdateBase
+}
+
+type RotationCreateOutput struct {
+	RotationId int `json:"rotation_id"`
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "goframe-shop/internal/packed"
+
+	_ "goframe-shop/internal/logic"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+
+	"github.com/gogf/gf/v2/os/gctx"
+
+	"goframe-shop/internal/cmd"
+)
+
+func main() {
+	cmd.Main.Run(gctx.GetInitCtx())
+}
