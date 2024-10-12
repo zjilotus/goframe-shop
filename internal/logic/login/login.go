99fc1d46851011451ab141dc7eb831f52848081d
ilotus <jilotus@qq.com>
2024-10-12 21:28:18 +0800

    登录鉴权_自定义中间件_session

---
+++ internal/logic/login/login.go
@@ -0,0 +1,41 @@
+package login
+
+import (
+	"context"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"goframe-shop/internal/dao"
+	"goframe-shop/internal/model"
+	"goframe-shop/internal/model/entity"
+	"goframe-shop/internal/service"
+	"goframe-shop/utility"
+)
+
+type sLogin struct{}
+
+func init() {
+	service.RegisterLogin(New())
+}
+
+func New() *sLogin {
+	return &sLogin{}
+}
+
+func (s *sLogin) Login(ctx context.Context, in model.UserLoginInput) error {
+	adminInfo := entity.AdminInfo{}
+	err := dao.AdminInfo.Ctx(ctx).Where("name", in.Name).Scan(&adminInfo)
+	if err != nil {
+		return err
+	}
+	if utility.EncryptPassword(in.Password, adminInfo.UserSalt) != adminInfo.Password {
+		return gerror.New("账号或密码不正确")
+	}
+	if err := service.Session().SetUser(ctx, &adminInfo); err != nil {
+		return err
+	}
+	service.BizCtx().SetUser(ctx, &model.ContextUser{
+		Id:      uint(adminInfo.Id),
+		Name:    adminInfo.Name,
+		IsAdmin: uint8(adminInfo.IsAdmin),
+	})
+	return nil
+}
