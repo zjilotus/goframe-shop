4caa3ce4614645fb7ffd96f7bc5917712a92dd52
ilotus <jilotus@qq.com>
2024-10-18 21:57:28 +0800

    完成商品规格管理开发

---
+++ internal/service/good.go
@@ -0,0 +1,39 @@
+// ================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// You can delete these comments if you wish manually maintain this interface file.
+// ================================================================================
+
+package service
+
+import (
+	"goframe-shop/internal/model"
+
+	"golang.org/x/net/context"
+)
+
+type (
+	IGoods interface {
+		Create(ctx context.Context, in model.GoodsCreateInput) (out model.GoodsCreateOutput, err error)
+		// Delete 删除
+		Delete(ctx context.Context, id uint) (err error)
+		// Update 修改
+		Update(ctx context.Context, in model.GoodsUpdateInput) error
+		// GetList 查询分类列表
+		GetList(ctx context.Context, in model.GoodsGetListInput) (out *model.GoodsGetListOutput, err error)
+	}
+)
+
+var (
+	localGoods IGoods
+)
+
+func Goods() IGoods {
+	if localGoods == nil {
+		panic("implement not found for interface IGoods, forgot register?")
+	}
+	return localGoods
+}
+
+func RegisterGoods(i IGoods) {
+	localGoods = i
+}
---
+++ internal/service/goods_options.go
@@ -0,0 +1,39 @@
+// ================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// You can delete these comments if you wish manually maintain this interface file.
+// ================================================================================
+
+package service
+
+import (
+	"goframe-shop/internal/model"
+
+	"golang.org/x/net/context"
+)
+
+type (
+	IGoodsOptions interface {
+		Create(ctx context.Context, in model.GoodsOptionsCreateInput) (out model.GoodsOptionsCreateOutput, err error)
+		// Delete 删除
+		Delete(ctx context.Context, id uint) (err error)
+		// Update 修改
+		Update(ctx context.Context, in model.GoodsOptionsUpdateInput) error
+		// GetList 查询分类列表
+		GetList(ctx context.Context, in model.GoodsOptionsGetListInput) (out *model.GoodsOptionsGetListOutput, err error)
+	}
+)
+
+var (
+	localGoodsOptions IGoodsOptions
+)
+
+func GoodsOptions() IGoodsOptions {
+	if localGoodsOptions == nil {
+		panic("implement not found for interface IGoodsOptions, forgot register?")
+	}
+	return localGoodsOptions
+}
+
+func RegisterGoodsOptions(i IGoodsOptions) {
+	localGoodsOptions = i
+}
